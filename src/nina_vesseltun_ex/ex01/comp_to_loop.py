51a04c371b36d1e2110ce9f6fe5a21b2b27270ab
NIna <nive@nmbu.no>
2019-09-25 10:37:38 +0200

    Fix ex01

--- src/nina_vesseltun_ex/ex01/comp_to_loop.py
+++ src/nina_vesseltun_ex/ex01/comp_to_loop.py
@@ -1,12 +1,14 @@
 def squares_by_comp(n):
     return [k**2 for k in range(n) if k % 3 == 1]
 
+
 def squares_by_loop(n):
     list_of_squares = []
     for k in range(n):
-        if k % 3 ==1:
+        if k % 3 == 1:
             list_of_squares.append(k**2)
     return list_of_squares
+
 
 if __name__ == '__main__':
     if squares_by_comp(10) != squares_by_loop(10):
