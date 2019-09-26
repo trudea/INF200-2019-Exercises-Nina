8b8e40b2f3693fee930bfab26fdd405b7bdb92a2
NIna <nive@nmbu.no>
2019-09-26 15:51:10 +0200

    Added test_single()

---
+++ src/nina_vesseltun_ex/ex03_project/test_median.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nina Mariann Vesseltun'
+__email__ = 'nive@nmbu.no'
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+
+
+def test_single():
+    dataset = [1]
+    assert median(dataset) == 1
+
