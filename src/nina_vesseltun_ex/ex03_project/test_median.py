82f599acff5c8ac469adf049c934320d224421d1
NIna <nive@nmbu.no>
2019-09-27 10:03:54 +0200

    Updated code

--- src/nina_vesseltun_ex/ex03_project/test_median.py
+++ src/nina_vesseltun_ex/ex03_project/test_median.py
@@ -35,10 +35,10 @@ def test_even():
 
 def test_order():
     unordered_dataset = [3, 4, 5, 1, 2]
-    ordered_dataset = [1, 2, 3, 4, 5]
+    ordered_dataset = [1, 2, 3, 4, 5] # burde brukt sorted?
     reversed_dataset = ordered_dataset[::-1]
-    assert median(unordered_dataset) == 3 and median(ordered_dataset) ==3 and
-    median(reversed_dataset) == 3
+    assert (median(unordered_dataset) == 3 and median(ordered_dataset) ==3 and
+    median(reversed_dataset) == 3)
 
 
 def test_req_empty():
