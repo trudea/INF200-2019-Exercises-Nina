ae1f978f6f2743d9954b738be1b6abc25612ee23
NIna <nive@nmbu.no>
2019-10-07 11:53:07 +0200

    Wrote single_game()

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -7,9 +7,14 @@ __email__ = 'nive@nmbu.no', 'elinbj@nmbu.no'
 import random
 
 def chute_or_ladder(position):
-    from_ladder = [1, 8, 36, 43, 49, 65, 68]
 
-    return new_position
+    from_position = [1, 8, 36, 43, 49, 65, 68, 24, 33, 42, 56, 64, 74, 87]
+    to_position = [40, 10, 52, 62, 79, 82, 85, 5, 3, 30, 37, 27, 12, 70]
+    if position in from_position:
+        new_position = to_position[from_position.index(position)]
+        return new_position
+    else:
+        return position
 
 
 def single_game(num_players):
@@ -28,17 +33,21 @@ def single_game(num_players):
     """
     player_position = []
     player_moves = []
-    for i in num_players:
+    for i in range(num_players):
         player_position.append(0)  # everybody starts at position 0
         player_moves.append(0)  # starts with 0 moves
     play = True
+    winning_moves = 0
     while play is True:
-        for player in num_players:
+        for player in range(num_players):
             dice = random.randint(1, 6)
             player_position[player] += dice
             player_moves[player] += 1
-
-
+            player_position[player] = chute_or_ladder(player_position[player])
+            if player_position[player] >= 90:
+                winning_moves = player_moves[player]
+                play = False
+    return winning_moves
 
 def multiple_games(num_games, num_players):
     """
@@ -76,3 +85,6 @@ def multi_game_experiment(num_games, num_players, seed):
     num_moves : list
         List with the numbedr of moves needed in each game.
     """
+
+if __name__ == '__main__':
+    print(single_game(3))
